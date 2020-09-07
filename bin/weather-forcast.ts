1cf5cb7652746af0238085972d33a83b3ba78655
shinyahashimoto <56911904+shinyahashimoto@users.noreply.github.com>
2020-09-08 08:37:37 +0900

    Delete weather-forcast.ts

--- bin/weather-forcast.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { WeatherForcastStack } from '../lib/weather-forcast-stack';
-
-const app = new cdk.App();
-new WeatherForcastStack(app, 'WeatherForcastStack');
